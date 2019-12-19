6e9423b494eb0271b65036cf760f8c361ed6706e
Karnak19 <basile64.v@gmail.com>
2019-12-19 22:30:19 +0100

    finished resolvers for Sign resource

--- backend/scripts/resync.js
+++ backend/scripts/resync.js
@@ -1,5 +1,7 @@
 const sequelize = require("../sequelize");
 require("../models/user");
+require("../models/sign");
+require("../sequelize/associations");
 
 (async () => {
   await sequelize.sync({ force: true });

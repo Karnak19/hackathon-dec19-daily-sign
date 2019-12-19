b9518606b185504073caa85dacf20c35608d8aa4
crashbtz <cedric.moncade@gmail.com>
2019-12-19 23:01:08 +0100

    router + start pad sign

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+import "./App.css";
+import Header from "./components/Header/Header";
+import Login from "./components/Login/Login";
+import Sign from "./components/Sign/Sign";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <AuthRoute isAuth={true} path="/login" component={Login} />
+          <AuthRoute isAuth={true} path="/sign" component={Sign} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function AuthRoute({ isAuth, component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (!isAuth) {
+          return <Redirect to="/" />;
+        } else {
+          return <Component {...props} />;
+        }
+      }}
+    />
   );
 }
 

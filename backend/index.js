d4182f1e54ee63c1ab846b9ad5620854de1e7acc
Karnak19 <basile64.v@gmail.com>
2019-12-19 10:12:30 +0100

    initialize first backend files

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 4000;
+const graphqlHttp = require("express-graphql");
+
+// Console Logging
+const chalk = require("chalk");
+const error = chalk.bold.red;
+const success = chalk.bold.green;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(require("./middlewares/awesomeLogger"));
+
+// GraphQL
+const graphQlSchemas = require("./graphql/schema");
+const graphQlResolvers = require("./graphql/resolvers");
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: graphQlSchemas,
+    rootValue: graphQlResolvers,
+    graphiql: true
+  })
+);
+
+async function main() {
+  await sequelize.sync();
+  try {
+    await sequelize.authenticate();
+    console.log(success("Connection successful."));
+    app.listen(PORT, err => {
+      if (err) {
+        throw new Error(error("Something bad happened ..."));
+      }
+      console.log(success(`Listening to ${PORT}.`));
+    });
+  } catch (err) {
+    console.error(error("Unable to reach database: "), err);
+  }
+}
+
+if (process.env.NODE_ENV !== "test") {
+  main();
+}

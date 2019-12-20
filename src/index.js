5352e876f1664c8264a87554a8312ad10ee9047a
Karnak19 <basile64.v@gmail.com>
2019-12-20 09:17:47 +0100

    add Redux and auth

--- src/index.js
+++ src/index.js
@@ -3,21 +3,31 @@ import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { BrowserRouter, Route } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import App from "./App";
+import { reducer } from "./reducers/reducer";
 
 import "bootswatch/dist/darkly/bootstrap.min.css";
 import "./index.scss";
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const client = new ApolloClient({
   uri: "http://localhost:8000"
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <BrowserRouter>
-      <Route path="/" component={App} />
-    </BrowserRouter>
-  </ApolloProvider>,
+  <Provider store={store}>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Route path="/" component={App} />
+      </BrowserRouter>
+    </ApolloProvider>
+  </Provider>,
   document.getElementById("root")
 );

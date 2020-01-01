ce11faa7c65c4644cabc6eb87f70896f31326ea0
Karnak19 <basile64.v@gmail.com>
2020-01-01 21:39:41 +0100

    added users listing page, with small int if they has signed today. Also added a small avatar inside the navbar

---
+++ src/components/Users/UserCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Col, Card, CardBody, CardImg, CardTitle, CardText } from "reactstrap";
+import moment from "moment";
+
+const imgStyle = {
+  alignSelf: "center",
+  width: "50%",
+  borderRadius: "50%"
+};
+const today = moment().dayOfYear();
+
+function UserCard({ uuid, firstName, lastName, avatar, Signs }) {
+  const hasSigned = Signs.some(el => el.date === today);
+  return (
+    <Col key={uuid} xs={12} sm={6} md={4} lg={3} className="mb-4">
+      <Card outline color={hasSigned ? "success" : "danger"}>
+        <CardImg src={avatar} alt={`${firstName} ${lastName}`} style={imgStyle} className="mt-3" />
+        <CardBody>
+          <CardTitle className="text-center">{`${firstName} ${lastName}`}</CardTitle>
+          <CardText className={hasSigned ? "text-success" : "text-danger"} style={{ fontSize: 13 }}>
+            {hasSigned ? "a signé !" : "n'a pas signé !"}
+          </CardText>
+        </CardBody>
+      </Card>
+    </Col>
+  );
+}
+
+export default UserCard;
---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Row, Spinner } from "reactstrap";
+import { useQuery } from "@apollo/react-hooks";
+import { GET_USERS } from "../../gql";
+import UserCard from "./UserCard";
+
+function Users() {
+  const { data, loading } = useQuery(GET_USERS);
+  return (
+    <Row>
+      {loading ? (
+        <Spinner />
+      ) : (
+        data &&
+        data.users.map(user => {
+          return <UserCard {...user} />;
+        })
+      )}
+    </Row>
+  );
+}
+
+export default Users;

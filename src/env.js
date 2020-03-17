91216a3b8377b9566c757cba3ae5b045f04159b3
Karnak19 <basile64.v@gmail.com>
2020-03-17 10:16:01 +0100

    removind dotenv from frontend as it's not needed with CRA template and add Procfile from backend

--- src/env.js
+++ src/env.js
@@ -1,4 +1,1 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const API_URL = process.env.API_URL || "http://192.168.172.131:8000";
+export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
